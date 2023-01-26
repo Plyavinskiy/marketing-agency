5b663e4e474ab2fd8a4e87c7a6def35ebc70afec
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2023-01-26 13:42:31 +0200

    ci: set up the routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Clients from "./pages/Clients";
+import Contact from "./pages/Contact";
+
 function App() {
-  return <div className="bg-red-500">Let's Get Started</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
