54bd6013876e12def33a622724ded6a217d94754
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2023-01-29 16:32:40 +0200

    style: add styles for the Footer navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Layout({ children }) {
+  const location = useLocation();
+
   const menuItems = [
     {
       title: "Home",
       path: "/",
-      icon: "",
+      icon: "ri-home-smile-line",
     },
     {
       title: "About",
       path: "/about",
-      icon: "",
+      icon: "ri-information-line",
     },
     {
       title: "Clients",
       path: "/clients",
-      icon: "",
+      icon: "ri-user-location-fill",
     },
     {
       title: "Contact",
       path: "/contact",
-      icon: "",
+      icon: "ri-contacts-line",
     },
   ];
 
@@ -34,14 +36,29 @@ function Layout({ children }) {
       <div className="fixed left-0 right-0 bottom-10">
         <div className="flex justify-center w-full">
           {menuItems.map((item, index) => (
-            <div key={item.title}
-              className={`px-20 py-5 bg-primary ${index === 0 && "rounded-l"} ${
-                index === menuItems.length - 1 && "rounded-r"
-              }`}
-            >
-              <Link to={item.path} className="text-secondary text-xl">
-                {item.title}
-              </Link>
+            <div key={item.title} className="flex flex-col items-center justify-end">
+              {location.pathname === item.path && (
+                <div className="flex flex-col items-center">
+                  <div className="h-5 w-10 bg-primary rounded-t-full"></div>
+                  <div className="h-5 w-20 bg-primary rounded-t-full text-center flex items-center justify-center">
+                    <i className={`${item.icon} text-xl text-white`}></i>
+                  </div>
+                </div>
+              )}
+              <div
+                className={`px-20 py-5 bg-primary ${
+                  index === 0 && "rounded-l-lg"
+                } ${
+                  index === menuItems.length - 1 && "rounded-r-lg"
+                } flex item-center justify-center space-x-2`}
+              >
+                {location.pathname !== item.path && (
+                  <i className={`${item.icon} text-secondary text-xl`}></i>
+                )}
+                <Link to={item.path} className="text-secondary text-xl">
+                  {item.title}
+                </Link>
+              </div>
             </div>
           ))}
         </div>
