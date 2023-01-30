87e05ea3e186198badd709fa77b7d48891c32f10
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2023-01-30 18:55:12 +0200

    feat: add the "Why Choose Us" section to the About Page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import Layout from "../../components/Layout";
 import Intro from "./Intro";
 import WhatWeDo from "./WhatWeDo";
+import WhyChooseUs from "./WhyChooseUs";
 
 function About() {
   return (
     <Layout>
       <Intro />
       <WhatWeDo />
+      <WhyChooseUs />
     </Layout>
   );
 }
