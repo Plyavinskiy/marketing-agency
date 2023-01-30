3aee0be01791dd00b2c4c0292f5b5bbd0d75a37c
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2023-01-30 09:48:36 +0200

    feat: add the Address section to the Contact Page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 import Layout from "../../components/Layout";
+import Address from "./Address";
 
 function Contact() {
   return (
     <Layout>
-      <div>Contact</div>
+      <div className="grid grid-cols-2 px-32">
+        <Address />
+      </div>
     </Layout>
   );
 }
