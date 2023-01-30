c3e2f46b12c906fdbd77abf2f72d8713ee0cbd67
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2023-01-30 21:50:11 +0200

    ci: add the "basename" prop to the BrowserRouter for deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
