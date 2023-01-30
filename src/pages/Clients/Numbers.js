fe24afdbf99c2805ea5e18d09e549181e73bbb41
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2023-01-30 06:09:38 +0200

    feat: add the Statistics section to the Clients Page

---
+++ src/pages/Clients/Numbers.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Numbers() {
+  return (
+    <div className="px-[152px] mt-20">
+      <h2 className="text-2xl text-gray-600 my-5">Until today we have provided our services to...</h2>
+      <div className="bg-secondary flex justify-between space-x-10 p-5 rounded">
+        <div className="flex flex-col items-center space-y-2">
+          <h2 className="text-9xl text-white">152</h2>
+          <h2 className="text-white text-2xl">Clients</h2>
+        </div>
+
+        <div className="flex flex-col items-center space-y-2">
+          <h2 className="text-9xl text-white">485</h2>
+          <h2 className="text-white text-2xl">Projects</h2>
+        </div>
+
+        <div className="flex flex-col items-center space-y-2">
+          <h2 className="text-9xl text-white">24</h2>
+          <h2 className="text-white text-2xl">Locations</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Numbers;
--- src/pages/Clients/index.js
+++ src/pages/Clients/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import Layout from "../../components/Layout";
 import Intro from "./Intro";
 import ClientsList from "./ClientsList";
+import Numbers from "./Numbers";
 
 function Clients() {
   return (
     <Layout>
       <Intro />
       <ClientsList />
+      <Numbers />
     </Layout>
   );
 }
