fb6d1e43aab894528695ffba31d9599d388aaeea
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2023-01-30 18:54:47 +0200

    feat: add the "What We Do" section to the About Page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Layout from "../../components/Layout";
 import Intro from "./Intro";
+import WhatWeDo from "./WhatWeDo";
 
 function About() {
   return (
     <Layout>
       <Intro />
+      <WhatWeDo />
     </Layout>
   );
 }
