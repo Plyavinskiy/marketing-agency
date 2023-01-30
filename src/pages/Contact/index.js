c1b1e9b2f673e090f29c07c6e734ce630729c55d
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2023-01-30 10:15:41 +0200

    feat: add the Contact Form section to the Contact Page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Layout from "../../components/Layout";
 import Address from "./Address";
+import ContactForm from "./ContactForm";
 
 function Contact() {
   return (
     <Layout>
-      <div className="grid grid-cols-2 px-32">
+      <div className="grid grid-cols-2 px-32 gap-20 mt-32">
         <Address />
+        <ContactForm />
       </div>
     </Layout>
   );
