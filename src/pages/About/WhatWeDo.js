8994506272da1325a94e541add5fee6413f2a493
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2023-01-30 11:52:38 +0200

    style: add the responsiveness to the What We Do section of the About Page

--- src/pages/About/WhatWeDo.js
+++ src/pages/About/WhatWeDo.js
@@ -30,16 +30,16 @@ const services = [
 
 function WhatWeDo() {
   return (
-    <div className="z-20">
-      <h2 className="text-secondary text-7xl text-center z-20 font-semibold">
+    <div className="z-20 sm:mt-10">
+      <h2 className="text-secondary text-7xl text-center z-20 font-semibold sm:text-5xl">
         What We Do?
       </h2>
       <div className="h-72 w-full bg-primary mt-12"></div>
-      <div className="grid grid-cols-4 gap-10 mx-32 -mt-40">
+      <div className="grid grid-cols-4 sm:grid-cols-1 gap-10 mx-32 sm:mx-0 -mt-40 sm:w-full sm:px-5">
         {services.map((service) => (
           <div
             key={service.title}
-            className="p-5 bg-white border shadow-lg flex flex-col items-center space-y-5 transform hover:scale-105 duration-500"
+            className="p-5 bg-white border shadow-lg flex flex-col items-center space-y-5 transform hover:scale-105 duration-500 sm:w-full"
           >
             <img src={service.icon} alt={service.title} className="h-16 w-16" />
             <h2 className="text-2xl">{service.title}</h2>
